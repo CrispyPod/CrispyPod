bcf33eee462a5c2facf00bfa32d82dfb518b0d2f
fengjiongmax <fengjiongmax@gmail.com>
2023-10-09 04:21:44 +0000

    render title with inherit color

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -14,7 +14,7 @@
 
 <div class="navbar">
 	<div class="flex-1">
-		<a class="btn btn-ghost normal-case text-xl text-primary-focus" href="/">{siteName}</a>
+		<a class="btn btn-ghost normal-case text-xl" href="/">{siteName}</a>
 	</div>
 	<div class="flex-none">
 		<ul class="menu menu-horizontal px-1">
